2a39af653db4a0c4d51815ccc6536ebdb320f875
Kanisorn-S <karamahati@gmail.com>
2024-04-02 17:17:53 +0700

    testing esp32

--- test/test.ino
+++ test/test.ino
@@ -1,10 +1,14 @@
 void setup() {
     Serial.begin(9600);
     Serial.println("Hello World!");
+    pinMode(32, OUTPUT);
 }
 
 void loop() {
-    Serial.println("Testing connection");
+    Serial.println("Connected");
+    digitalWrite(32, HIGH);
+    delay(1000);
+    digitalWrite(32, LOW);
     delay(1000);
 }
 

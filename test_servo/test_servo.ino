d8b24647102ff0d6cfae50282efd2ea1a0367088
Kanisorn-S <karamahati@gmail.com>
2024-05-04 16:51:09 +0700

    add testing for servo motors

---
+++ test_servo/test_servo.ino
@@ -0,0 +1,33 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+const int servo_max_cw = 98; // full throtle clockwise 
+const int servo_min_cw = 350; // min throtle counter-clockwise 
+const int servo_max_ccw = 632; // full throtle counter-clockwise 
+const int servo_min_ccw = 380; // min throtle counter-clockwise 
+const int mini_cw = 60;
+const int mini_ccw = 1100;
+
+Adafruit_PWMServoDriver board = Adafruit_PWMServoDriver(0x40);
+
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println("4 channel Servo test");
+
+    board.begin();
+    board.setPWMFreq(60);
+}
+
+void loop() {
+    Serial.println("Entering Loop");
+    float t = 5000;
+    float start_time = millis();
+    board.setPWM(0, 0, servo_min_cw);
+    while (millis() - start_time < t) {
+        Serial.print(millis());
+        Serial.println("Spinning");
+    }
+    board.setPWM(0, 0, 0);
+    sleep(10000000);
+}
